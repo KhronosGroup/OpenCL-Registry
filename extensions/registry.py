67b05dc4d0b894cc3c65855b62b6a69012fa05ba
Jon Leech <oddhack@sonic.net>
2017-01-26 04:53:32 -0800

    Make structure and registry files more consistent with GL registry. Move extension number registry into extensions/registry.py and autogenerate the HTML index, instead of registering numbers in cl.xml and requiring a separate manual update of index.php.

---
+++ extensions/registry.py
@@ -0,0 +1,254 @@
+registry = {
+# OpenGL extension number and name registry
+
+    'cl_altera_compiler_mode' : {
+        'number' : 30,
+        'flags' : { 'public' },
+        'url' : 'extensions/altera/cl_altera_compiler_mode.txt',
+    },
+    'cl_altera_device_temperature' : {
+        'number' : 29,
+        'flags' : { 'public' },
+        'url' : 'extensions/altera/cl_altera_device_temperature.txt',
+    },
+    'cl_altera_live_object_tracking' : {
+        'number' : 28,
+        'flags' : { 'public' },
+        'url' : 'extensions/altera/cl_altera_live_object_tracking.txt',
+    },
+    'cl_amd_bus_addressable_memory' : {
+        'number' : 25,
+        'flags' : { 'public' },
+        'url' : 'extensions/amd/cl_amd_bus_addressable_memory.txt',
+    },
+    'cl_amd_device_attribute_query' : {
+        'number' : 7,
+        'flags' : { 'public' },
+        'url' : 'extensions/amd/cl_amd_device_attribute_query.txt',
+    },
+    'cl_amd_fp64' : {
+        'number' : 8,
+        'flags' : { 'public' },
+        'url' : 'extensions/amd/cl_amd_fp64.txt',
+    },
+    'cl_amd_media_ops' : {
+        'number' : 9,
+        'flags' : { 'public' },
+        'url' : 'extensions/amd/cl_amd_media_ops.txt',
+    },
+    'cl_amd_media_ops2' : {
+        'number' : 15,
+        'flags' : { 'public' },
+        'url' : 'extensions/amd/cl_amd_media_ops2.txt',
+    },
+    'cl_arm_core_id' : {
+        'number' : 26,
+        'flags' : { 'public' },
+        'url' : 'extensions/arm/cl_arm_get_core_id.txt',
+    },
+    'cl_arm_import_memory' : {
+        'number' : 38,
+        'flags' : { 'public' },
+        'url' : 'extensions/arm/cl_arm_import_memory.txt',
+    },
+    'cl_arm_non_uniform_work_group_size' : {
+        'number' : 39,
+        'flags' : { 'public' },
+        'url' : 'extensions/arm/cl_arm_non_uniform_work_group_size.txt',
+    },
+    'cl_arm_printf' : {
+        'number' : 27,
+        'flags' : { 'public' },
+        'url' : 'extensions/arm/cl_arm_printf.txt',
+    },
+    'cl_arm_shared_virtual_memory' : {
+        'number' : 40,
+        'flags' : { 'public' },
+        'url' : 'extensions/arm/cl_arm_shared_virtual_memory.txt',
+    },
+    'cl_arm_thread_limit_hint' : {
+        'number' : 41,
+        'flags' : { 'public' },
+        'url' : 'extensions/arm/cl_arm_thread_limit_hint.txt',
+    },
+    'cl_ext_atomic_counters_32' : {
+        'number' : 12,
+        'flags' : { 'public' },
+        'url' : 'extensions/ext/cl_ext_atomic_counters_32.txt',
+    },
+    'cl_ext_atomic_counters_64' : {
+        'number' : 13,
+        'flags' : { 'public' },
+        'url' : 'extensions/ext/cl_ext_atomic_counters_64.txt',
+    },
+    'cl_ext_device_fission' : {
+        'number' : 11,
+        'flags' : { 'public' },
+        'url' : 'extensions/ext/cl_ext_device_fission.txt',
+    },
+    'cl_ext_migrate_memobject' : {
+        'number' : 10,
+        'flags' : { 'public' },
+        'url' : 'extensions/ext/cl_ext_migrate_memobject.txt',
+    },
+    'cl_img_cached_allocations' : {
+        'number' : 44,
+        'flags' : { 'public' },
+        'url' : 'extensions/img/cl_img_cached_allocations.txt',
+    },
+    'cl_img_use_gralloc_ptr' : {
+        'number' : 45,
+        'flags' : { 'public' },
+        'url' : 'extensions/img/cl_img_use_gralloc_ptr.txt',
+    },
+    'cl_img_yuv_image' : {
+        'number' : 46,
+        'flags' : { 'public' },
+        'url' : 'extensions/img/cl_img_yuv_image.txt',
+    },
+    'cl_intel_accelerator' : {
+        'number' : 24,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_accelerator.txt',
+    },
+    'cl_intel_advanced_motion_estimation' : {
+        'number' : 33,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_advanced_motion_estimation.txt',
+    },
+    'cl_intel_d3d11_nv12_media_sharing' : {
+        'number' : 31,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_d3d11_nv12_media_sharing.txt',
+    },
+    'cl_intel_device_partition_by_names' : {
+        'number' : 20,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_device_partition_by_names.txt',
+    },
+    'cl_intel_device_side_avc_motion_estimation' : {
+        'number' : 50,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_device_side_avc_motion_estimation.txt',
+    },
+    'cl_intel_driver_diagnostics' : {
+        'number' : 47,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_driver_diagnostics.txt',
+    },
+    'cl_intel_dx9_media_sharing' : {
+        'number' : 14,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_dx9_media_sharing.txt',
+    },
+    'cl_intel_egl_image_yuv' : {
+        'number' : 37,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_egl_image_yuv.txt',
+    },
+    'cl_intel_motion_estimation' : {
+        'number' : 23,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_motion_estimation.txt',
+    },
+    'cl_intel_packed_yuv' : {
+        'number' : 42,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_packed_yuv.txt',
+    },
+    'cl_intel_planar_yuv' : {
+        'number' : 49,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_planar_yuv.txt',
+    },
+    'cl_intel_required_subgroup_size' : {
+        'number' : 43,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_required_subgroup_size.txt',
+    },
+    'cl_intel_simultaneous_sharing' : {
+        'number' : 34,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_simultaneous_sharing.txt',
+    },
+    'cl_intel_subgroups' : {
+        'number' : 35,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_subgroups.txt',
+    },
+    'cl_intel_subgroups_short' : {
+        'number' : 48,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_subgroups_short.txt',
+    },
+    'cl_intel_thread_local_exec' : {
+        'number' : 16,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_thread_local_exec.txt',
+    },
+    'cl_intel_va_api_media_sharing' : {
+        'number' : 36,
+        'flags' : { 'public' },
+        'url' : 'extensions/intel/cl_intel_va_api_media_sharing.txt',
+    },
+    'cl_khr_d3d10_sharing' : {
+        'number' : 6,
+        'flags' : { 'public' },
+        'url' : 'extensions/khr/cl_khr_d3d10_sharing.txt',
+    },
+    'cl_khr_gl_sharing' : {
+        'number' : 1,
+        'flags' : { 'public' },
+        'url' : 'extensions/khr/cl_khr_gl_sharing.txt',
+    },
+    'cl_khr_icd' : {
+        'number' : 5,
+        'flags' : { 'public' },
+        'url' : 'extensions/khr/cl_khr_icd.txt',
+    },
+    'cl_nv_compiler_options' : {
+        'number' : 17,
+        'flags' : { 'public' },
+        'url' : 'extensions/nv/cl_nv_compiler_options.txt',
+    },
+    'cl_nv_d3d10_sharing' : {
+        'number' : 3,
+        'flags' : { 'public' },
+        'url' : 'extensions/nv/cl_nv_d3d10_sharing.txt',
+    },
+    'cl_nv_d3d11_sharing' : {
+        'number' : 4,
+        'flags' : { 'public' },
+        'url' : 'extensions/nv/cl_nv_d3d11_sharing.txt',
+    },
+    'cl_nv_d3d9_sharing' : {
+        'number' : 2,
+        'flags' : { 'public' },
+        'url' : 'extensions/nv/cl_nv_d3d9_sharing.txt',
+    },
+    'cl_nv_device_attribute_query' : {
+        'number' : 18,
+        'flags' : { 'public' },
+        'url' : 'extensions/nv/cl_nv_device_attribute_query.txt',
+    },
+    'cl_nv_pragma_unroll' : {
+        'number' : 19,
+        'flags' : { 'public' },
+        'url' : 'extensions/nv/cl_nv_pragma_unroll.txt',
+    },
+    'cl_qcom_android_native_buffer_host_ptr' : {
+        'number' : 32,
+        'flags' : { 'public' },
+        'url' : 'extensions/qcom/cl_qcom_android_native_buffer_host_ptr.txt',
+    },
+    'cl_qcom_ext_host_ptr' : {
+        'number' : 21,
+        'flags' : { 'public' },
+        'url' : 'extensions/qcom/cl_qcom_ext_host_ptr.txt',
+    },
+    'cl_qcom_ion_host_ptr' : {
+        'number' : 22,
+        'flags' : { 'public' },
+        'url' : 'extensions/qcom/cl_qcom_ion_host_ptr.txt',
+    },
+}
---
+++ extensions/sortreg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+import copy, os, re, string, sys
+import printreg
+import registry
+
+printreg.printreg(registry.registry, 'registry')
