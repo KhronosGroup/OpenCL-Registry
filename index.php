801daef969356862df5b808da931fe5ed01df57b
Ben Ashbaugh <ben.ashbaugh@intel.com>
2021-07-20 23:08:44 -0700

    add KHR extensions to the main registry page
    added manually for now, but could generate in the future.

--- index.php
+++ index.php
@@ -249,8 +249,69 @@ include_once("../../assets/static_pages/khr_page_top.php");
     updated placeholder.)</p>
 
 
+    <h6> <a name="khrextspecs"></a>
+     Khronos Extension Specifications</h6>
+
+<p> Khronos extensions are published in the OpenCL Extension Specification.
+    These links are to the appropriate chapter of the OpenCL Extension
+    Specification.</p>
+
+<ul>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_3d_image_writes">cl_khr_3d_image_writes</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_async_work_group_copy_fence">cl_khr_async_work_group_copy_fence</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_byte_addressable_store">cl_khr_byte_addressable_store</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_create_command_queue">cl_khr_create_command_queue</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_d3d10_sharing">cl_khr_d3d10_sharing</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_d3d11_sharing">cl_khr_d3d11_sharing</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_depth_images">cl_khr_depth_images</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_device_enqueue_local_arg_types">cl_khr_device_enqueue_local_arg_types</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_device_uuid">cl_khr_device_uuid</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_dx9_media_sharing">cl_khr_dx9_media_sharing</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_egl_event">cl_khr_egl_event</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_egl_image">cl_khr_egl_image</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_extended_async_copies">cl_khr_extended_async_copies</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_extended_bit_ops">cl_khr_extended_bit_ops</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_extended_versioning">cl_khr_extended_versioning</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_fp16">cl_khr_fp16</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_fp64">cl_khr_fp64</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_depth_images">cl_khr_gl_depth_images</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_event">cl_khr_gl_event</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_msaa_sharing">cl_khr_gl_msaa_sharing</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_sharing">cl_khr_gl_sharing</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_global_int32_base_atomics</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_global_int32_extended_atomics</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_icd-opencl">cl_khr_icd</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_il_program">cl_khr_il_program</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_image2d_from_buffer">cl_khr_image2d_from_buffer</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_initialize_memory">cl_khr_initialize_memory</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int64_atomics">cl_khr_int64_base_atomics</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int64_atomics">cl_khr_int64_extended_atomics</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_local_int32_base_atomics</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_local_int32_extended_atomics</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_integer_dot_product">cl_khr_integer_dot_product</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_mipmap_image">cl_khr_mipmap_image</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_mipmap_image">cl_khr_mipmap_image_writes</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_pci_bus_info">cl_khr_pci_bus_info</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_priority_hints">cl_khr_priority_hints</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_select_fprounding_mode">cl_khr_select_fprounding_mode</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_spir">cl_khr_spir</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_srgb_image_writes">cl_khr_srgb_image_writes</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroups">cl_khr_subgroups</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_ballot">cl_khr_subgroup_ballot</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_clustered_reduce">cl_khr_subgroup_clustered_reduce</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_extended_types">cl_khr_subgroup_extended_types</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_named_barrier">cl_khr_subgroup_named_barrier</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_non_uniform_arithmetic">cl_khr_subgroup_non_uniform_arithmetic</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_non_uniform_vote">cl_khr_subgroup_non_uniform_vote</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_shuffle">cl_khr_subgroup_shuffle</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_shuffle_relative">cl_khr_subgroup_shuffle_relative</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_suggested_local_work_size">cl_khr_suggested_local_work_size</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_terminate_context">cl_khr_terminate_context</a></li>
+<li><a href="https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_throttle_hints">cl_khr_throttle_hints</a></li>
+</ul>
+
 <h6> <a name="otherextspecs"></a>
-     Extension Specifications</h6>
+     Vendor and Multi-Vendor Extension Specifications</h6>
 
 <?php include("extensions/clext.php"); ?>
 
