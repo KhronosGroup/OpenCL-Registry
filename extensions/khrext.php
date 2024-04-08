e408e85f00441c82c6fc408e448e4eccca628abe
Jon Leech <4693344+oddhack@users.noreply.github.com>
2024-04-08 15:40:24 -0700

    Change khr extension links in the registry to point to the OpenCL API spec, not the OpenCL Extensions spec, since they have been moved. (#152)

--- extensions/khrext.php
+++ extensions/khrext.php
@@ -1,138 +1,138 @@
 <ul>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_3d_image_writes">cl_khr_3d_image_writes</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_3d_image_writes">cl_khr_3d_image_writes</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_async_work_group_copy_fence">cl_khr_async_work_group_copy_fence</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_async_work_group_copy_fence">cl_khr_async_work_group_copy_fence</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_byte_addressable_store">cl_khr_byte_addressable_store</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_byte_addressable_store">cl_khr_byte_addressable_store</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_command_buffer">cl_khr_command_buffer</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_command_buffer">cl_khr_command_buffer</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_command_buffer_multi_device">cl_khr_command_buffer_multi_device</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_command_buffer_multi_device">cl_khr_command_buffer_multi_device</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_command_buffer_mutable_dispatch">cl_khr_command_buffer_mutable_dispatch</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_command_buffer_mutable_dispatch">cl_khr_command_buffer_mutable_dispatch</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_create_command_queue">cl_khr_create_command_queue</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_create_command_queue">cl_khr_create_command_queue</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_d3d10_sharing">cl_khr_d3d10_sharing</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_d3d10_sharing">cl_khr_d3d10_sharing</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_d3d11_sharing">cl_khr_d3d11_sharing</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_d3d11_sharing">cl_khr_d3d11_sharing</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_depth_images">cl_khr_depth_images</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_depth_images">cl_khr_depth_images</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_device_enqueue_local_arg_types">cl_khr_device_enqueue_local_arg_types</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_device_enqueue_local_arg_types">cl_khr_device_enqueue_local_arg_types</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_device_uuid">cl_khr_device_uuid</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_device_uuid">cl_khr_device_uuid</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_dx9_media_sharing">cl_khr_dx9_media_sharing</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_dx9_media_sharing">cl_khr_dx9_media_sharing</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_egl_event">cl_khr_egl_event</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_egl_event">cl_khr_egl_event</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_egl_image">cl_khr_egl_image</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_egl_image">cl_khr_egl_image</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_expect_assume">cl_khr_expect_assume</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_expect_assume">cl_khr_expect_assume</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_extended_async_copies">cl_khr_extended_async_copies</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_extended_async_copies">cl_khr_extended_async_copies</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_extended_bit_ops">cl_khr_extended_bit_ops</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_extended_bit_ops">cl_khr_extended_bit_ops</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_extended_versioning">cl_khr_extended_versioning</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_extended_versioning">cl_khr_extended_versioning</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_memory">cl_khr_external_memory</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_memory">cl_khr_external_memory</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_memory">cl_khr_external_memory_dma_buf</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_memory">cl_khr_external_memory_dma_buf</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_memory">cl_khr_external_memory_dx</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_memory">cl_khr_external_memory_dx</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_memory">cl_khr_external_memory_opaque_fd</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_memory">cl_khr_external_memory_opaque_fd</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_semaphore">cl_khr_external_memory_win32</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_semaphore">cl_khr_external_memory_win32</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_semaphore">cl_khr_external_semaphore</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_semaphore">cl_khr_external_semaphore</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_semaphore">cl_khr_external_semaphore_dx_fence</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_semaphore">cl_khr_external_semaphore_dx_fence</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_semaphore">cl_khr_external_semaphore_opaque_fd</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_semaphore">cl_khr_external_semaphore_opaque_fd</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_external_semaphore">cl_khr_external_semaphore_sync_fd</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_external_semaphore">cl_khr_external_semaphore_sync_fd</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_fp16">cl_khr_fp16</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_fp16">cl_khr_fp16</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_fp64">cl_khr_fp64</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_fp64">cl_khr_fp64</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_depth_images">cl_khr_gl_depth_images</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_gl_depth_images">cl_khr_gl_depth_images</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_event">cl_khr_gl_event</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_gl_event">cl_khr_gl_event</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_msaa_sharing">cl_khr_gl_msaa_sharing</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_gl_msaa_sharing">cl_khr_gl_msaa_sharing</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_gl_sharing">cl_khr_gl_sharing</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_gl_sharing">cl_khr_gl_sharing</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_global_int32_base_atomics</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_int32_atomics">cl_khr_global_int32_base_atomics</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_global_int32_extended_atomics</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_int32_atomics">cl_khr_global_int32_extended_atomics</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_icd-opencl">cl_khr_icd</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_icd-opencl">cl_khr_icd</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_il_program">cl_khr_il_program</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_il_program">cl_khr_il_program</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_image2d_from_buffer">cl_khr_image2d_from_buffer</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_image2d_from_buffer">cl_khr_image2d_from_buffer</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_initialize_memory">cl_khr_initialize_memory</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_initialize_memory">cl_khr_initialize_memory</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int64_atomics">cl_khr_int64_base_atomics</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_int64_atomics">cl_khr_int64_base_atomics</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int64_atomics">cl_khr_int64_extended_atomics</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_int64_atomics">cl_khr_int64_extended_atomics</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_integer_dot_product">cl_khr_integer_dot_product</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_integer_dot_product">cl_khr_integer_dot_product</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_kernel_clock">cl_khr_kernel_clock</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_kernel_clock">cl_khr_kernel_clock</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_local_int32_base_atomics</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_int32_atomics">cl_khr_local_int32_base_atomics</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_int32_atomics">cl_khr_local_int32_extended_atomics</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_int32_atomics">cl_khr_local_int32_extended_atomics</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_mipmap_image">cl_khr_mipmap_image</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_mipmap_image">cl_khr_mipmap_image</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_mipmap_image">cl_khr_mipmap_image_writes</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_mipmap_image">cl_khr_mipmap_image_writes</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_pci_bus_info">cl_khr_pci_bus_info</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_pci_bus_info">cl_khr_pci_bus_info</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_priority_hints">cl_khr_priority_hints</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_priority_hints">cl_khr_priority_hints</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_select_fprounding_mode">cl_khr_select_fprounding_mode</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_select_fprounding_mode">cl_khr_select_fprounding_mode</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_semaphore">cl_khr_semaphore</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_semaphore">cl_khr_semaphore</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_spir">cl_khr_spir</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_spir">cl_khr_spir</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_srgb_image_writes">cl_khr_srgb_image_writes</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_srgb_image_writes">cl_khr_srgb_image_writes</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_ballot">cl_khr_subgroup_ballot</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_ballot">cl_khr_subgroup_ballot</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_clustered_reduce">cl_khr_subgroup_clustered_reduce</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_clustered_reduce">cl_khr_subgroup_clustered_reduce</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_extended_types">cl_khr_subgroup_extended_types</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_extended_types">cl_khr_subgroup_extended_types</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_named_barrier">cl_khr_subgroup_named_barrier</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_named_barrier">cl_khr_subgroup_named_barrier</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_non_uniform_arithmetic">cl_khr_subgroup_non_uniform_arithmetic</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_non_uniform_arithmetic">cl_khr_subgroup_non_uniform_arithmetic</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_non_uniform_vote">cl_khr_subgroup_non_uniform_vote</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_non_uniform_vote">cl_khr_subgroup_non_uniform_vote</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_rotate">cl_khr_subgroup_rotate</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_rotate">cl_khr_subgroup_rotate</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_shuffle">cl_khr_subgroup_shuffle</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_shuffle">cl_khr_subgroup_shuffle</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroup_shuffle_relative">cl_khr_subgroup_shuffle_relative</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroup_shuffle_relative">cl_khr_subgroup_shuffle_relative</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_subgroups">cl_khr_subgroups</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_subgroups">cl_khr_subgroups</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_suggested_local_work_size">cl_khr_suggested_local_work_size</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_suggested_local_work_size">cl_khr_suggested_local_work_size</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_terminate_context">cl_khr_terminate_context</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_terminate_context">cl_khr_terminate_context</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_throttle_hints">cl_khr_throttle_hints</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_throttle_hints">cl_khr_throttle_hints</a>
 </li>
-<li><a href="specs/3.0-unified/html/OpenCL_Ext.html#cl_khr_work_group_uniform_arithmetic">cl_khr_work_group_uniform_arithmetic</a>
+<li><a href="specs/3.0-unified/html/OpenCL_API.html#cl_khr_work_group_uniform_arithmetic">cl_khr_work_group_uniform_arithmetic</a>
 </li>
 </ul>
